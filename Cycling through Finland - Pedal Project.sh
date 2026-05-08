e336a5249815443428c6b7c2288527b98a8d0943
claraslopez <clarasantosuva@gmail.com>
2026-05-08 12:38:45 +0200

    rasberry files

---
+++ Cycling through Finland - Pedal Project.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+printf '\033c\033]0;%s\a' Cycling through Finland - Pedal Project
+base_path="$(dirname "$(realpath "$0")")"
+"$base_path/Cycling through Finland - Pedal Project.arm64" "$@"
